bc3d3de7d5f844a8d1dc3c37b697a8b254fd061e
Dmytro Moisieiev <dmoisieiev@readdle.com>
2022-03-26 20:45:44 +0200

    refactoring

--- exercises/exercise-163.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-163) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; List-of-Fahrenheit -> List-of-Celsius
-(check-expect (convertFC (cons 0 (cons 10 '()))) (cons -17.7 (cons -12.2 '()))) 
-(define (convertFC lof)
-  (cond
-    [(empty? lof) '()]
-    [else (cons (* (- (first lof) 32) (/ 5 9)) (convertFC (rest lof)))]))
--- exercises/exercise-184.rkt
+++
@@ -1,6 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname exercise-184) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(list (string=? "a" "b") #false)
-(list (+ 10 20) (* 10 20) (/ 10 20))
-(list "dana" "jane" "mary" "laura")
