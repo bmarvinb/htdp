b00c2d13177b4f4a46f5c859acff206b3665cde0
Dmytro Moisieiev <dmoisieiev@readdle.com>
2022-03-27 16:01:21 +0300

    trees completed

--- IV. Interwined Data/trees.rkt
+++ IV. Interwined Data/trees.rkt
@@ -25,6 +25,7 @@
 ; Youngest Generation: 
 (define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
 
+; Exercise 310. Develop count-persons. The function consumes a family tree and counts the child structures in the tree. 
 ; FT -> Number
 ; counts children
 
@@ -38,28 +39,72 @@
                     (count-persons (child-father ft))
                     (count-persons (child-mother ft)))]))
 
+; Exercise 311. Develop the function average-age.
+; It consumes a family tree and the current year.
+; It produces the average age of all child structures in the family tree.
 ; FT Date -> Number
 ; count an average age
 
-;(define (average-age ftree year)
-;  ())
+(check-expect (average-age Gustav 2022)
+              68)
 
-;(define (count-persons ftree)
-;  (cond
-;    [(no-parent? ftree) 0]
-;    [(child? ftree) (+ 1
-;                       (count-persons (child-father ftree))
-;                       (count-persons (child-mother ftree)))]))
+(define (average-age ft year)
+  (local (
+          (define (years ft year)
+            (cond
+              [(no-parent? ft) '()]
+              [else (cons (- year (child-date ft))
+                          (append (years (child-mother ft) year)
+                                  (years (child-father ft) year)))]))
 
+          (define YEARS (years ft year))
+          (define TOTAL (foldl (lambda (acc curr) (+ acc curr)) 0 YEARS)))
+    (round (/ TOTAL
+              (length YEARS)))))
+
+; Exercise 312. Develop the function eye-colors,
+; which consumes a family tree and produces a list of all eye colors in the tree.
 ; FT -> List-of-strings
-; returns list of all eye colors in the tree
 
-;(define (eye-colors ftree)
-;  ())
+(check-expect (eye-colors Gustav)
+              (list "brown" "blue" "green" "green" "pink"))
+
+(define (eye-colors ft)
+  (cond
+    [(no-parent? ft) '()]
+    [else (cons (child-eyes ft)
+                (append (eye-colors (child-mother ft))
+                        (eye-colors (child-father ft))))]))
+
+
+; Exercise 313. Suppose we need the function blue-eyed-ancestor?,
+; which is like blue-eyed-child? but responds with #true only when a proper ancestor, not the given child itself, has blue eyes.
 
 
 ; FT -> Boolean
-; return #true only when a proper ancestor
+(check-expect (blue-eyed-ancestor? Gustav)
+              #t)
 
-;(define (blue-eyed-ancestor? ftree)
-;  ())
+(check-expect (blue-eyed-ancestor? Eva)
+              #f)
+
+(define (blue-eyed-ancestor? ft)
+  (local (
+          (define (blue-eyes? ft)
+            (cond
+              [(no-parent? ft) #f]
+              [else (string=? (child-eyes ft) "blue")]))
+          )
+
+    (cond
+      [(no-parent? ft) #false]
+      [(blue-eyes? (child-father ft)) #t]
+      [(blue-eyes? (child-mother ft)) #t]
+      [else
+       (or
+        (blue-eyed-ancestor? (child-father ft))
+        (blue-eyed-ancestor? (child-mother ft)))])
+    ))
+
+
+
