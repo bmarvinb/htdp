e4de3d6067bc9335be56abf1dfd45dadef2938b4
Dmytro Moisieiev <dmoisieiev@readdle.com>
2022-04-16 11:51:21 +0300

    Worm game completed

--- II. Arbitrarily Large Data/worm.rkt
+++ II. Arbitrarily Large Data/worm.rkt
@@ -16,7 +16,7 @@
 
 (define WORM-IMG (square 10 "solid" "red"))
 (define FOOD-IMG (square 10 "solid" "green"))
-  
+
 ;; =================
 ;; Data definitions:
 
@@ -28,6 +28,10 @@
 ;; Food is (make-food Number Number)
 ;; interp. the food at position x, y
 
+(define-struct cell (x-pos y-pos))
+;; EmptyCell is (make-cell Number Number)
+;; interp. the empty cell mean cell that doesn't contain form part or food
+
 (define-struct worm (parts food direction))
 ;; Worm is (make-worm List-of-WormPart Food String)
 ;; interp. the worm store worm parts, food and worm direction
@@ -35,18 +39,80 @@
 ;; =================
 ;; Functions:
 
+;; List-of-Any -> Any | #f
+(check-expect (index-of '(1 2 3 4) 0) 1)
+(check-expect (index-of '(1 2 3 4) 2) 3)
+(check-expect (index-of '(1 2 3 4) 5) #f)
+(define (index-of lst target)
+  (local (
+          (define (fn-for-index-of lst index)
+            (cond
+              [(empty? lst) #f]
+              [(= index target) (first lst)]
+              [else (fn-for-index-of (rest lst) (+ index 1))]))
+          )
+    (fn-for-index-of lst 0)))
+
 ;; List-of-WormPart -> WormPart
-(check-expect (last-worm-part (cons (make-worm-part 0 0) (cons (make-worm-part 0 10) empty)))
+(check-expect (worm-head (cons (make-worm-part 0 0) (cons (make-worm-part 0 10) empty)))
               (make-worm-part 0 10))
-(define (last-worm-part low)
+(define (worm-head lowp)
   (cond
-    [(empty? (rest low)) (first low)]
-    [else (last-worm-part (rest low))]))
+    [(empty? (rest lowp)) (first lowp)]
+    [else (worm-head (rest lowp))]))
+
+;; List-of-WormPart -> List-of-WormPart
+(check-expect (worm-tail (cons (make-worm-part 0 0) empty))
+              empty)
+
+(check-expect (worm-tail (cons (make-worm-part 0 0)
+                               (cons (make-worm-part 0 10)
+                                     (cons (make-worm-part 0 20) empty))))
+              (cons (make-worm-part 0 0)
+                    (cons (make-worm-part 0 10) empty)))
+
+(define (worm-tail lowp)
+  (if (empty? (rest lowp))
+      empty
+      (cons (first lowp) (worm-tail (rest lowp)))))
+
 
 ;; List-of-WormPart -> Food
-;; !!!
-(define (random-food low)
-  (make-food 70 70))
+;; generate new food in random position
+(define (random-food lowp)
+  (local (
+          ;; List-of-WormPart Number Number -> Boolean
+          ;; determine is this available position for food
+          (define (empty-space? lowp x-pos y-pos)
+            (cond
+              [(empty? lowp) #t]
+              [(and (= (worm-part-x-pos (first lowp)) x-pos)
+                    (= (worm-part-y-pos (first lowp)) y-pos)) #f]
+              [else (empty-space? (rest lowp) x-pos y-pos)]))
+
+          ;; Number Number -> List-of-Numbers
+          ;; generate sequence of positions
+          (define (positions size step)
+            (cond
+              [(<= size 10) (cons 10 empty)]
+              [else (cons size (positions (- size step) step))]))
+
+          ;; List-of-Number -> Number
+          (define (random-pos lop)
+            (index-of lop (random (length lop))))
+
+          (define STEP 10)
+          (define RANDOM-X-POS (random-pos (positions (- WORLD-WIDTH STEP) STEP)))
+          (define RANDOM-Y-POS (random-pos (positions (- WORLD-HEIGHT STEP) STEP))))
+
+    (cond
+      [(empty-space? lowp RANDOM-X-POS RANDOM-Y-POS) (make-food RANDOM-X-POS RANDOM-Y-POS)]
+      [else (random-food lowp)])))
+
+;; List-of-WormPart WormPart -> List-of-WormPart
+;; concat new worm head and tail
+(define (concat lowp wp)
+  (append lowp (cons wp empty)))
 
 ;; List-of-WormPart direction -> List-of-WormPart
 ;; update worm parts position coordinates base on direction
@@ -59,7 +125,7 @@
 (check-expect (move-worm (cons (make-worm-part 0 0) (cons (make-worm-part 10 0) (cons (make-worm-part 20 0) empty))) "down")
               (cons (make-worm-part 10 0) (cons (make-worm-part 20 0) (cons (make-worm-part 20 10) empty))))
 
-(define (move-worm low direction)
+(define (move-worm lowp direction)
   (local (
           ;; WormPart -> WormPart
           (define (change-y-pos wp dx)
@@ -69,26 +135,25 @@
           (define (change-x-pos wp dx)
             (make-worm-part (dx (worm-part-x-pos wp) 10) (worm-part-y-pos wp)))
 
-          ; String List-of-WormPart -> List-of-WormPart
-          (define (updated-worms direction low)
+          ; List-of-WormPart KeyEvent -> List-of-WormPart
+          (define (updated-worms lowp direction)
 
-            (local ((define WORM-HEAD (last-worm-part low)))
+            (local ((define WORM-HEAD (worm-head lowp)))
               (cond
                 [(string=? direction "up") (change-y-pos WORM-HEAD -)]
                 [(string=? direction "down") (change-y-pos WORM-HEAD +)]
                 [(string=? direction "right") (change-x-pos WORM-HEAD +)]
                 [(string=? direction "left") (change-x-pos WORM-HEAD -)]
-              [else low]))))
+                [else lowp]))))
     (cond
-      [(empty? low) empty]
-      [else (append (rest low)
-                    (cons (updated-worms direction low) empty))])))
+      [(empty? lowp) empty]
+      [else (concat (rest lowp) (updated-worms lowp direction))])))
 
-;; Food List-of-WormPart -> Boolean
+;; Food WormPart -> Boolean
 ;; return true if worm is eat food
-(define (can-eat? food low)
-  (and (= (food-x-pos food) (worm-part-x-pos (last-worm-part low)))
-       (= (food-y-pos food) (worm-part-y-pos (last-worm-part low)))))
+(define (can-eat? food wp)
+  (and (= (food-x-pos food) (worm-part-x-pos wp))
+       (= (food-y-pos food) (worm-part-y-pos wp))))
 
 ;; Food -> WormPart
 ;; return WormPart that transformed from Food
@@ -99,9 +164,9 @@
 ;; update worm parts and food position
 (define (tock w)
   (cond
-    [(can-eat? (worm-food w) (worm-parts w))
-     (make-worm (move-worm (append (cons (eat (worm-food w)) empty) (worm-parts w)) (worm-direction w))
-                (random-food (worm-parts w))
+    [(can-eat? (worm-food w) (worm-head (worm-parts w)))
+     (make-worm (move-worm (concat (worm-parts w) (eat (worm-food w))) (worm-direction w))
+                (random-food (concat (worm-parts w) (eat (worm-food w))))
                 (worm-direction w))]
     [else
      (make-worm (move-worm (worm-parts w) (worm-direction w))
@@ -117,22 +182,25 @@
                (render-worm (worm-parts w))))
 
 ;; List-of-WormPart -> Image
-;; return worm parts images
+;; render worm parts
 (check-expect (render-worm empty) MTS)
 (check-expect (render-worm (cons (make-worm-part 0 0) (cons (make-worm-part 0 10) empty)))
               (place-image WORM-IMG 5 5 (place-image WORM-IMG 5 15 MTS)))
-(define (render-worm low)
+(define (render-worm lowp)
   (cond
-    [(empty? low) MTS]
+    [(empty? lowp) MTS]
     [else (place-image WORM-IMG
-                       (+ 5 (worm-part-x-pos (first low)))
-                       (+ 5 (worm-part-y-pos (first low)))
-                       (render-worm (rest low)))]))
+                       (+ 5 (worm-part-x-pos (first lowp)))
+                       (+ 5 (worm-part-y-pos (first lowp)))
+                       (render-worm (rest lowp)))]))
 
 ;; Worm KeyEvent -> Worm
 ;; !!!
 (define (handle-key w ke)
   (cond
+    [(key=? ke "escape") (make-worm (cons (make-worm-part 0 0) empty)
+                                (random-food (cons (make-worm-part 0 0) empty))
+                                "down")]
     [(or (string=? (worm-direction w) "up") (string=? (worm-direction w) "down"))
      (cond
        [(key=? ke "right") (make-worm (worm-parts w) (worm-food w) "right")]
@@ -147,13 +215,28 @@
      ]
     [else w]))
 
-;; Worm -> Boolean 
-;; !!!
+;; Worm -> Boolean
+;; determine is head hit the wall
+(define (hit-the-wall? w)
+  (or (> (worm-part-y-pos (worm-head (worm-parts w))) WORLD-HEIGHT)
+      (< (worm-part-y-pos (worm-head (worm-parts w))) 0)
+      (> (worm-part-x-pos (worm-head (worm-parts w))) WORLD-WIDTH)
+      (< (worm-part-x-pos (worm-head (worm-parts w))) 0)))
+
+;; List-of-WormPart WormPart -> Boolean
+;; determine is worm eat himself
+(define (eat-himself? lowp wp)
+  (>= (length (filter (lambda (x) (and (= (worm-part-x-pos x) (worm-part-x-pos wp))
+                                       (= (worm-part-y-pos x) (worm-part-y-pos wp))))
+                      lowp))
+      1))
+
+;; Worm -> Boolean
 (define (stop-game? w)
-  (or (> (worm-part-y-pos (last-worm-part (worm-parts w))) WORLD-HEIGHT)
-      (< (worm-part-y-pos (last-worm-part (worm-parts w))) 0)
-      (> (worm-part-x-pos (last-worm-part (worm-parts w))) WORLD-WIDTH)
-      (< (worm-part-x-pos (last-worm-part (worm-parts w))) 0)))
+  (cond
+    [(hit-the-wall? w) #t]
+    [(eat-himself? (worm-tail (worm-parts w)) (worm-head (worm-parts w))) #t]
+    [else #f]))
 
 ;; Worm -> Worm
 (define (main w)
@@ -161,7 +244,8 @@
     (on-tick tock 0.1)
     (on-key handle-key)
     (to-draw render)
-    )) ; (stop-when stop-game?)
+    (stop-when stop-game?)))
 
-(define WORM-1 (make-worm (cons (make-worm-part 0 0) (cons (make-worm-part 0 10) empty)) (make-food 50 50) "down"))
-(main WORM-1)
+(main (make-worm (cons (make-worm-part 0 0) empty)
+                 (random-food (cons (make-worm-part 0 0) empty))
+                 "down"))
