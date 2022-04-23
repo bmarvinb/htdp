4e348aec75ca535f8b5013c7f9e6f41f2ca82e61
Dmitry Moiseev <mdmitry94@gmail.com>
2022-04-23 12:59:28 +0300

    Interpreting variables

--- IV. Interwined Data/interpreting-expressions.rkt
+++ IV. Interwined Data/interpreting-expressions.rkt
@@ -40,7 +40,6 @@
 (check-expect (eval-bool-expression (make-bool-not #t)) #f)
 (check-expect (eval-bool-expression (make-bool-not #f)) #t)
 (check-expect (eval-bool-expression (make-bool-and #t (make-bool-not #f))) #t)
-
 (define (eval-bool-expression exp)
   (cond
     [(boolean? exp) exp]
@@ -50,4 +49,182 @@
     [(bool-or? exp) (or (eval-bool-expression (bool-or-left exp))
                         (eval-bool-expression (bool-or-right exp)))]))
 
- 
+; Exercise 349. Create tests for parse
+; S-expr -> Boolean
+(define (atom? s)
+  (or (number? s)
+      (string? s)
+      (symbol? s)))
+
+(define WRONG "Invalid BSL-expr")
+
+; S-expr -> BSL-expr
+(check-expect (parse '1) '1)
+(check-expect (parse '(+ 10 -10)) (make-add 10 -10))
+(check-expect (parse '(+ (* 5 5) 25)) (make-add (make-mul 5 5) 25))
+(check-error (parse '(sqr 4)) WRONG)
+(check-error (parse 'x) WRONG)
+(check-error (parse "hello world") WRONG)
+(check-error (parse '(1 2 3)) WRONG)
+(check-error (parse '(string-append "hello" "world")) WRONG)
+(define (parse s)
+  (local (
+          ; Atom -> BSL-expr 
+          (define (parse-atom s)
+            (cond
+              [(number? s) s]
+              [(string? s) (error WRONG)]
+              [(symbol? s) (error WRONG)]))
+
+          ; SL -> Boolean
+          (define (consists-of-3 s)
+            (and (cons? s) (cons? (rest s)) (cons? (rest (rest s)))
+                 (empty? (rest (rest (rest s))))))
+          
+          ; SL -> BSL-expr
+          (define (parse-sl s)
+            (cond
+              [(and (consists-of-3 s) (symbol? (first s)))
+               (cond
+                 [(symbol=? (first s) '+)
+                  (make-add (parse (second s)) (parse (third s)))]
+                 [(symbol=? (first s) '*)
+                  (make-mul (parse (second s)) (parse (third s)))]
+                 [else (error WRONG)])]
+              [else (error WRONG)])))
+    (cond
+      [(atom? s) (parse-atom s)]
+      [else (parse-sl s)])))
+
+
+; S-expr -> Number
+(check-expect (interpreter-expr '(+ (* 5 5) 25)) 50)
+(define (interpreter-expr s)
+  (cond
+    [(string? (parse s)) (parse s)]
+    [else (eval-expression (parse s))]))
+
+; A BSL-var-expr is one of: 
+; – Number
+; – Symbol 
+; – (make-add BSL-var-expr BSL-var-expr)
+; – (make-mul BSL-var-expr BSL-var-expr)
+
+; Exercise 352. Design subst.
+; The function consumes a BSL-var-expr ex, a Symbol x, and a Number v.
+; It produces a BSL-var-expr like ex with all occurrences of x replaced by v.
+
+; BSL-var-expr Symbol Number -> BSL-var-expr
+(check-expect (subst 1 'x 2) 1)
+(check-expect (subst 'y 'x 1) 'y)
+(check-expect (subst 'x 'x 1) 1)
+(check-expect (subst (make-add 'x 2) 'x 1)
+              (make-add 1 2))
+(check-expect (subst (make-add (make-add 'x 2) 2) 'x 1)
+              (make-add (make-add 1 2) 2))
+(define (subst ex x v)
+  (cond
+    [(number? ex) ex]
+    [(symbol? ex) (if (eq? ex x) v ex)]
+    [(add? ex) (make-add (subst (add-left ex) x v)
+                         (subst (add-right ex) x v))]
+    [(mul? ex) (make-mul (subst (mul-left ex) x v)
+                         (subst (mul-right ex) x v))]
+    [else ex]))
+
+; Exercise 353. Design the numeric? function. It determines whether a BSL-var-expr is also a BSL-expr. 
+(check-expect (numeric? 1) #true)
+(check-expect (numeric? 'x) #false)
+(check-expect (numeric? (make-add 'x 1)) #false) 
+(check-expect (numeric? (make-add (make-add 1 2) 3)) #true)
+
+(define (numeric? ex)
+  (cond
+    [(symbol? ex) #false]
+    [(number? ex) #true]
+    [(add? ex) (and (numeric? (add-left ex))
+                    (numeric? (add-right ex)))]
+    [(mul? ex) (and (numeric? (mul-left ex))
+                    (numeric? (mul-right ex)))]
+    [else #false]))
+
+(define ERROR-MESSAGE "Not a numeric value")
+
+; Exercise 354. Design eval-variable.
+(check-expect (eval-variable 1) 1)
+(check-error (eval-variable 'x) ERROR-MESSAGE)
+(check-error (eval-variable (make-add 'x 1)) ERROR-MESSAGE)
+(check-expect (eval-variable (make-add (make-add 1 1) 2)) 4)
+
+(define (eval-variable ex)
+  (cond
+    [(numeric? ex) (eval-expression ex)]
+    [else (error "Not a numeric value")]))
+
+
+; An AL (short for association list) is [List-of Association].
+; An Association is a list of two items:
+;   (cons Symbol (cons Number '())).
+
+(define AL '((x 1) (y 2) (z 3)))
+
+(check-expect (eval-variable* (make-add 'x 'y) AL) 3)
+
+; BSL-var-expr AL -> Number
+(check-expect (eval-variable* (make-add 'x 'y) AL) 3)
+(check-expect (eval-variable* (make-add (make-add 'x 'x) 'y) AL) 4)
+(check-expect (eval-variable* (make-mul (make-add 'x 'x) 'y) AL) 4)
+(check-error (eval-variable* (make-mul (make-add 'a 'x) 'y) AL) ERROR-MESSAGE)
+
+(define (eval-variable* ex da)
+  (local ((define SUBSTITUTED-EXPRESSION (subst-all ex da)))
+    (cond
+      [(numeric? SUBSTITUTED-EXPRESSION) (eval-expression SUBSTITUTED-EXPRESSION)]
+      [else (error ERROR-MESSAGE)])))
+
+(check-expect (subst-all (make-add 'x 'y) AL) (make-add 1 2))
+(check-expect (subst-all (make-add (make-add 'x 'x) 'y) AL) (make-add (make-add 1 1) 2))
+(check-expect (subst-all (make-mul (make-add 'x 'x) 'y) AL) (make-mul (make-add 1 1) 2))
+
+(define (subst-all ex da)
+  (cond
+    [(empty? da) ex]
+    [else (subst-all (subst ex (first (first da)) (second (first da)))
+                     (rest da))]))
+
+(check-expect (eval-var-lookup (make-add 'x 'y) AL) 3)
+(check-expect (eval-var-lookup (make-add (make-add 'x 'x) 'y) AL) 4)
+(check-expect (eval-var-lookup (make-mul (make-add 'x 'x) 'y) AL) 4)
+(check-error (eval-var-lookup (make-mul (make-add 'a 'x) 'y) AL) UNDEFINED-ERROR)
+
+(define (eval-var-lookup ex da)
+  (local ((define SUBSTITUTED-EXPRESSION (replace-variables ex da)))
+    (cond
+      [(numeric? SUBSTITUTED-EXPRESSION) (eval-expression SUBSTITUTED-EXPRESSION)]
+      [else (error SUBSTITUTED-EXPRESSION)])))
+
+(define UNDEFINED-ERROR "Variable undefined")
+
+; BSL-var-expr AL -> BSL-var-expr
+(check-expect (replace-variables (make-add 'x 'y) AL) (make-add 1 2))
+(check-expect (replace-variables (make-add (make-add 'x 'x) 'y) AL) (make-add (make-add 1 1) 2))
+
+(define (replace-variables ex da)
+  (cond
+    [(empty? ex) '()]
+    [(symbol? ex) (replace-variable ex)]
+    [(add? ex) (make-add (replace-variable (add-left ex) da)
+                         (replace-variable (add-right ex) da))]
+    [(mul? ex) (make-mul (replace-variable (mul-left ex) da)
+                         (replace-variable (mul-right ex) da))]
+    [else ex]))
+  
+(define (replace-variable ex da)
+  (cond
+    [(symbol? ex) (if (boolean? (assq ex da))
+                      (error UNDEFINED-ERROR)
+                      (second (assq ex da)))]
+    [else (replace-variables ex da)]))
+
+
+
--- IV. Interwined Data/s-expressions.rkt
+++ IV. Interwined Data/s-expressions.rkt
@@ -1,6 +1,26 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname s-expressions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+          
+
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol 
+
+; S-expr -> Boolean
+(define (atom? s)
+  (or (number? s)
+      (string? s)
+      (symbol? s)))
+
 ; S-expr Symbol -> N 
 ; counts all occurrences of sy in sexp
 (check-expect (count 'world 'hello) 0)
@@ -8,15 +28,7 @@
 (check-expect (count '(((world) hello) hello) 'hello) 2)
 (define (count sexp sy)
   (local (
-          ; S-expr -> Boolean
-          (define (atom? sexp)
-            (cond
-              [(number? sexp) #t]
-              [(string? sexp) #t]
-              [(symbol? sexp) #t]
-              [else #f]))
-
-          ; SL -> N 
+          ; SL Symbol -> N 
           ; counts all occurrences of sy in sl 
           (define (count-sl sl)
             (cond
@@ -24,16 +36,67 @@
               [else
                (+ (count (first sl) sy) (count-sl (rest sl)))]))
  
-          ; Atom -> N 
+          ; Atom Symbol -> N
           ; counts all occurrences of sy in at 
           (define (count-atom at)
             (cond
               [(number? at) 0]
               [(string? at) 0]
-              [(symbol? at) (if (symbol=? at sy) 1 0)]))
-          )
+              [(symbol? at) (if (symbol=? at sy) 1 0)])))
     (cond
       [(atom? sexp) (count-atom sexp)]
-      [else (count-sl sexp)]))
-  )
-  
+      [else (count-sl sexp)])))
+
+; Exercise 318. Design depth.
+; The function consumes an S-expression and determines its depth.
+; An Atom has a depth of 1. The depth of a list of S-expressions is the maximum depth of its items plus 1.
+; S-expr -> Number
+(check-expect (depth 10) 1)
+(check-expect (depth '( "a" 1)) 2)
+(check-expect (depth '((1 2))) 3)
+(define (depth sexp)
+  (local (
+          ; SL Symbol -> Number
+          (define (sl-depth sl)
+            (cond
+              [(empty? sl) 1]
+              [else (max (depth (first sl))
+                         (sl-depth (rest sl)))])))
+    (cond
+      [(atom? sexp) 1]
+      [else (add1 (sl-depth sexp))])))
+
+; Exercise 319. Design substitute.
+; It consumes an S-expression s and two symbols, old and new.
+; The result is like s with all occurrences of old replaced by new.
+; S-expr Symbol Symbol -> S-expr
+(check-expect (substitute 'hello 'hello 'goodbye) 'goodbye)
+(check-expect (substitute 'hello 'world 'goodbye) 'hello)
+(check-expect (substitute '(hello world) 'hello 'goodbye) '(goodbye world))
+(check-expect (substitute '(hello (awesome world)) 'awesome 'awful) '(hello (awful world)))
+(define (substitute sexp old new)
+  (local (
+          ; SL -> SL
+          (define (map-sl sl)
+            (cond
+              [(empty? sl) '()]
+              [else (cons (substitute (first sl) old new)
+                          (map-sl (rest sl)))])))
+    (cond
+      [(atom? sexp) (if (eq? sexp old) new sexp)]
+      [else (map-sl sexp)])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
