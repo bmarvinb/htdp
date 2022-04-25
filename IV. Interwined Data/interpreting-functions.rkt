20cca196e388a9bc09fd1e288d208a7148d2c7e6
Dmitry Moiseev <mdmitry94@icloud.com>
2022-04-25 09:15:26 +0300

    Interpreting Functions

--- IV. Interwined Data/interpreting-functions.rkt
+++ IV. Interwined Data/interpreting-functions.rkt
@@ -16,10 +16,11 @@
 (define FUNCTION-NOT-DEFINED "Function not defined")
 (define SYMBOL-NOT-FOUND "Symbol not found")
 
-;; BSL-var-expr Symbol Number -> BSL-var-expr
-;; implemented in exercise 352
+;; BSL-var-expr Symbol Number -> BSL-fun-expr
 (check-expect (subst (make-add 'x 2) 'x 2)
               (make-add 2 2))
+(check-expect (subst (make-fun 'f (make-mul 2 'x)) 'x 4)
+              (make-fun 'f (make-mul 2 4)))
 (define (subst ex x v)
   (cond
     [(number? ex) ex]
@@ -28,10 +29,11 @@
                          (subst (add-right ex) x v))]
     [(mul? ex) (make-mul (subst (mul-left ex) x v)
                          (subst (mul-right ex) x v))]
+    [(fun? ex) (make-fun (fun-name ex) (subst (fun-arg ex) x v))]
     [else ex]))
 
 ;; Exercise 357. Design eval-definition1
-;; BSL-fun-expr Symbol Symbol BSL-fun-expr
+;; BSL-fun-expr Symbol Symbol BSL-fun-expr -> Number
 ;; interpret. f represents a function name
 ;;            x represents a function parameter
 ;;            b represents a function body
@@ -50,3 +52,49 @@
                  (define plugd (subst b x value)))
            (eval-definition1 plugd f x b))
          (error FUNCTION-NOT-DEFINED))]))
+
+;; Exercise 358. Provide a structure type and a data definition for function definitions
+
+(define-struct fun-def [name param body])
+;; BSL-fun-def is:
+;; (make-fun-def Symbol Symbol BSL-fun-expr)
+
+(define f (make-fun-def 'f 'x (make-add 3 'x)))
+(define g (make-fun-def 'g 'y (make-fun 'f (make-mul 2 'y))))
+(define h (make-fun-def 'h 'v (make-add (make-fun 'f 'v) (make-fun 'g 'v))))
+
+;; BSL-fun-def* is [List-of BSL-fun-def]
+(define da-fgh (list f g h))
+
+; BSL-fun-def* Symbol -> BSL-fun-def
+; retrieves the definition of f in da
+; signals an error if there is none
+(check-expect (lookup-def da-fgh 'g) g)
+(check-error (lookup-def da-fgh 'x) FUNCTION-NOT-DEFINED)
+(define (lookup-def da f)
+  (cond
+    [(empty? da) (error FUNCTION-NOT-DEFINED)]
+    [else (if (eq? (fun-def-name (first da)) f)
+              (first da)
+              (lookup-def (rest da) f))]))
+
+;; Exercise 359. Design eval-function*
+;; BSL-fun-expr BSL-fun-def* -> Number
+(check-expect (eval-function* (make-fun 'f 4) da-fgh) 7)
+(check-expect (eval-function* (make-fun 'g 1) da-fgh) 5)
+(check-expect (eval-function* (make-fun 'h 1) da-fgh) 9)
+(define (eval-function* ex da)
+  (cond
+    [(number? ex) ex]
+    [(symbol? ex) (error SYMBOL-NOT-FOUND)]
+    [(add? ex) (+ (eval-function* (add-left ex) da)
+                  (eval-function* (add-right ex) da))]
+    [(mul? ex) (* (eval-function* (mul-left ex) da)
+                  (eval-function* (mul-right ex) da))]
+    [(fun? ex)
+     (local ((define fn (lookup-def da (fun-name ex))))
+     (if (not (empty? fn))
+         (local ((define value (eval-function* (fun-arg ex) da))
+                 (define plugd (subst (fun-def-body fn) (fun-def-param fn) (eval-function* (fun-arg ex) da))))
+           (eval-function* plugd da))
+         (error FUNCTION-NOT-DEFINED)))]))
